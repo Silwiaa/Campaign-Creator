515a15dac4566e70f4bee7d31482b2c6396f775c
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-05-18 18:16:09 +0200

    campaignfundmapper refactored

--- src/main/java/com/recruitmenttask/campaincreator/mapper/CampaignFundMapper.java
+++ src/main/java/com/recruitmenttask/campaincreator/mapper/CampaignFundMapper.java
@@ -2,20 +2,17 @@ package com.recruitmenttask.campaincreator.mapper;
 
 import com.recruitmenttask.campaincreator.domain.CampaignFund;
 import com.recruitmenttask.campaincreator.domain.CampaignFundDto;
-import com.recruitmenttask.campaincreator.exceptiond.CampaignNotFoundException;
+import com.recruitmenttask.campaincreator.exception.CampaignNotFoundException;
 import com.recruitmenttask.campaincreator.service.CampaignService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
 public class CampaignFundMapper {
-    private final CampaignService campaignService;
-
-    public CampaignFund mapToCampaignFund(CampaignFundDto campaignFundDto) throws CampaignNotFoundException {
-        return CampaignFund.builder()
-                .foundId(campaignFundDto.getFoundId())
-                .campaign(campaignService.findCampaignByID(campaignFundDto.getCampaignId()))
+public CampaignFundDto mapToCampaignFundDto(CampaignFund campaignFund) {
+        return CampaignFundDto.builder()
+                .foundId(campaignFund.getFoundId())
+                .campaignId(campaignFund.getCampaign().getCampaignId())
                 .build();
     }
 }
