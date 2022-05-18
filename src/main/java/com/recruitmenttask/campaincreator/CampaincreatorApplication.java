556d892d2cb3449e185709fdf9f199b838d590cd
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-05-18 15:40:04 +0200

    Packages added to the project

---
+++ src/main/java/com/recruitmenttask/campaincreator/CampaincreatorApplication.java
@@ -0,0 +1,13 @@
+package com.recruitmenttask.campaincreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaincreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampaincreatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/recruitmenttask/campaincreator/CampaincreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitmenttask.campaincreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaincreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
