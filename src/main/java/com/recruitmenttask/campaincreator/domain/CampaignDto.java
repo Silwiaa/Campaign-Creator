fe2eff29473f2052a84ba4e7c0c4f1c5c354fd26
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-05-18 15:54:05 +0200

    Fields added to CampaignDto

--- src/main/java/com/recruitmenttask/campaincreator/domain/CampaignDto.java
+++ src/main/java/com/recruitmenttask/campaincreator/domain/CampaignDto.java
@@ -1,4 +1,22 @@
 package com.recruitmenttask.campaincreator.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Builder
+@Getter
+@AllArgsConstructor
 public class CampaignDto {
+    private Long campaignId;
+    private String name;
+    private BigDecimal bidAmount;
+    private String campaignFound;
+    private Status status;
+    private String town;
+    private BigDecimal radius;
 }
