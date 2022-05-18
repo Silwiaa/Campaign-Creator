b28f7d0e8d02c7e6c2a58a9b814538c91cd819be
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-05-18 16:03:12 +0200

    CampainMapper class created, added mapToCampaign and mapToCampainDto methods

---
+++ src/main/java/com/recruitmenttask/campaincreator/mapper/CampaignMapper.java
@@ -0,0 +1,32 @@
+package com.recruitmenttask.campaincreator.mapper;
+
+import com.recruitmenttask.campaincreator.domain.Campaign;
+import com.recruitmenttask.campaincreator.domain.CampaignDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CampaignMapper {
+    public Campaign mapToCampain(CampaignDto campaignDto) {
+        return Campaign.builder()
+                .campaignId(campaignDto.getCampaignId())
+                .name(campaignDto.getName())
+                .bidAmount(campaignDto.getBidAmount())
+                .campaignFound(campaignDto.getCampaignFound())
+                .status(campaignDto.getStatus())
+                .town(campaignDto.getTown())
+                .radius(campaignDto.getRadius())
+                .build();
+    }
+
+    public CampaignDto mapToCampainDto(Campaign campaign) {
+        return CampaignDto.builder()
+                .campaignId(campaign.getCampaignId())
+                .name(campaign.getName())
+                .bidAmount(campaign.getBidAmount())
+                .campaignFound(campaign.getCampaignFound())
+                .status(campaign.getStatus())
+                .town(campaign.getTown())
+                .radius(campaign.getRadius())
+                .build();
+    }
+}
