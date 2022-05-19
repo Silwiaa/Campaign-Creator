0231efd3cd458041688d03016bc05067b478aedb
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-05-19 09:58:34 +0200

    EmeraldMapper refactored

--- src/main/java/com/recruitmenttask/campaincreator/mapper/EmeraldMapper.java
+++ src/main/java/com/recruitmenttask/campaincreator/mapper/EmeraldMapper.java
@@ -1,8 +1,11 @@
 package com.recruitmenttask.campaincreator.mapper;
 
+import com.recruitmenttask.campaincreator.domain.Campaign;
 import com.recruitmenttask.campaincreator.domain.Emerald;
 import com.recruitmenttask.campaincreator.domain.EmeraldDto;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Service
 public class EmeraldMapper {
@@ -10,6 +13,7 @@ public EmeraldDto mapToEmeraldDto(Emerald emerald) {
         return EmeraldDto.builder()
                 .emeraldId(emerald.getEmeraldId())
                 .balance(emerald.getBalance())
+                .campaignIds(emerald.getCampaigns().stream().map(Campaign::getCampaignId).collect(Collectors.toList()))
                 .build();
     }
 }
