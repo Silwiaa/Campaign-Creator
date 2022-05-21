de5e43d06bb4efd5227378af4d539e793c776f9f
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-05-21 14:21:16 +0200

    added

--- src/main/java/com/recruitmenttask/campaincreator/repository/EmeraldRepository.java
+++ src/main/java/com/recruitmenttask/campaincreator/repository/EmeraldRepository.java
@@ -2,6 +2,8 @@ package com.recruitmenttask.campaincreator.repository;
 
 import com.recruitmenttask.campaincreator.domain.Emerald;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmeraldRepository extends CrudRepository<Emerald, Long> {
 }
--- src/main/java/com/recruitmenttask/campaincreator/repository/KeywordRepository.java
+++ src/main/java/com/recruitmenttask/campaincreator/repository/KeywordRepository.java
@@ -2,6 +2,8 @@ package com.recruitmenttask.campaincreator.repository;
 
 import com.recruitmenttask.campaincreator.domain.Keyword;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface KeywordRepository extends CrudRepository<Keyword, Long> {
 }
